e86975f0bb360f21b64fcd2c4ef70c1e5a617c99
Lastdier <a499340016@gmail.com>
2016-10-31 15:47:21 +0800

    添加了自定义词典
    添加了自定义词典，使得分词器能将汽车品牌独立分出

--- methods.py
+++ methods.py
@@ -1,7 +1,21 @@
 import jieba.posseg
 
 
+jieba.set_dictionary('data/my_dict.csv')
+words_that_should_not_be_divided = ['召回','自燃','短轴','看上去','EA1111.4T','大帕萨特','B7.5','高尔夫大使','女子高尔夫','5000','AS5174','PM2.5',
+        '奥迪量产','DL382','中华区','PLA3.0','申联阳光','双龙大道','双龙山森林公园','双龙寺','大众汽车集团',
+        '捷豹路虎汽车','EA211','大本田','ESP','特斯拉杀手','1.4TFSI','大众style','小辉腾','EVO150.4','V6发动机',
+        'v6发动机','NEX12.2','MC261','200T','变形金刚','小康生活','东风标志','PassatA','媒体大众','350牛米','现代感',
+        '标志性','马丁脸','前麦弗逊','现代化','PQ34','EA88','PQ35','荣威新能源','马自达工厂','上海通用工厂',
+        '捷克姆拉达','标致大道','大众一家','上海大众汽车','大众赛车','C级车',
+        'C级豪华','C级轿跑','C级别','高尔夫运动','别克4S店', '长安', '小夏', '长城', '大切', '凯泽西', '发现四', '大宝',
+        '小理', '小四', '小海', '大发', '大奔', '大众', '小八', '拉达', '小七', '小科', '老马', '长马', '汉能', '迷你',
+        '起亚']
+for l in words_that_should_not_be_divided:
+    jieba.suggest_freq(l,True)
 # 删除不需要的词，将剩余的词转换为list
+
+
 def word_filter(string):
     result = []                     # 以list装载数据
     words = jieba.posseg.cut(string)
@@ -15,8 +29,6 @@ def word_filter(string):
         elif 'q' == flag:
             pass
         elif 'm' == flag:
-            pass
-        elif 'eng' == flag:
             pass
         else:
             result.append(word)
