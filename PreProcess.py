c89e0ed4c3cf4b38b523eef80c68c27bee9bb894
Lastdier <a499340016@gmail.com>
2016-12-05 23:21:15 +0800

    word2vec作200维度词向量，snownlp情感分析提供1个特征维度，bigram、thigram、unigram各提供1个特征维度，用GBDT进行三分类 线下f1: 76.9

---
+++ PreProcess.py
@@ -0,0 +1,371 @@
+#getStopWords.py 得到停用词表
+#getDict.py 做特征提取,得到情感词典
+#getView.py 做视角提取,得到视角词表
+from methods import *
+import math
+import re
+import jieba.posseg
+import time
+from snownlp import SnowNLP
+
+#得到一个完整的基础视角词表,输入是AllViews:由两个Label.csv中获取的视角词+拓展的视角词组成，输出是所有视角词组成的字典
+def getFirstView(labelname):
+    # 存储从Label.csv中获得的视角词
+    views={}
+    lists=['208','335','560','630','730','3008','5008']
+    for word in lists:
+        views[word]=1
+    #获取Label中的视角词
+    label_file = open(labelname, encoding='utf-8')
+    label_data = label_file.readlines()
+    label_file.close()
+    for line in label_data:
+        line = line.strip()
+        content=line
+        zhPattern1 = re.compile(u'[\u4e00-\u9fa5]+')
+        match1 = zhPattern1.search(content)
+        if match1:
+            #包含中文的视角词不可拆,为视角词拓展做准备
+            jieba.suggest_freq(content, True)
+        if(content.isdigit()==False):
+            views[content]=1
+    return views
+
+#删除一些拓展出来的视角词，主要针对英文，比如拓展的视角词为:k2k3k4，但是实际上k2、k3、k4本身是视角词，所以k2k3k4属于误判
+def removewSpecilaView(str,SpecialViews):
+    temp=str
+    SpecialView=list(SpecialViews)
+    SpecialView.sort(key=lambda x: len(x), reverse=True)
+    for view in SpecialView:
+        if(str.__contains__(view)):
+            str=str.replace(view,'')
+    if(str==''):
+        temp=str
+    return temp
+
+#根据现有的视角词后面出现英文，数字，-,·来扩展视角词
+#基本拓展完毕，除了视角词纯粹是英文/数字的还没有做，其他的完成了。目前的功能就只是把一些不要的视角词删了，然后输出到Views.csv
+def getSecondView(views,testname,ViewPathname):
+    newviews = {}
+    View_First = open(ViewPathname, 'w', encoding='utf-8')
+    '''
+    label_file = open(testname,encoding='utf-8')
+    label_data = label_file.readlines()
+    label_file.close()
+    for line in label_data:
+        line = line.strip()
+        line = line.split('\t')
+        content = line[1]
+        temp = 0
+        count = 0
+        newview = ''
+        # 视角词在最后会提取不到，加。
+        content = content + '。'
+        pattern = r'\d+[款|年|版|台|月|日|周|寸|缸|辆|副|轮|键|T]|\d+:\d+|\d\.\d|\d+连冠|\d+公里|\d+小时|http:\s+|201\d|\d+km/h|[\d+|\d+.\d+|.\d+]+[t|起|万|元]'
+        lists = ['4s', 'pk', 'performance', '1-','1,6']
+        for word in lists:
+            content = content.replace(word, '')
+        for word in re.findall(pattern, content):
+            content = content.replace(word, '')
+        for word in jieba.cut(content):
+            count += 1
+            if (views.get(word) is not None):
+                if (newview == ''):
+                    temp = count
+                    newview = word
+                    continue
+            else:
+                if (newview != '') and (count - temp) == 1:
+                    if re.match('^[0-9a-zA-Z]+$', word):
+                        word = removewSpecilaView(word, views)
+                    if (re.match('^[0-9a-zA-Z]+$', word) and word.isdigit()==False) or word == '-' or word == '系' or word == '·' or word == ' ' or word=='－' or word=='\0xa':
+                        temp = count
+                        newview += word
+                    else:
+                        if re.match('^[\w]+[-|·]+$', newview):
+                            newview = newview.replace('-', '')
+                        if re.match('^[\w]+[·]+$', newview):
+                            newview = newview.replace('·', '')
+                        if re.match('^[\w]+[ ]+$', newview):
+                            newview = newview.replace(' ', '')
+                        newview = newview.replace('http', '')
+                        if (views.get(newview) is None and newviews.get(newview) is None and newview.isdigit() == False and newview.isalpha() == False):
+                            newviews[newview] = 1
+                            if(newview.__contains__('-') or newview.__contains__('·') or newview.__contains__(' ') or newview.__contains__('\0xa')):
+                                #View_First.write(newview + '\n')
+                                 #views[newview] = 1
+                                 print(newview)
+                                 newview = ''
+    '''
+    #去除重复的视角词
+    views=list(set(list(views)))
+    #列表按长度降序排序
+    views.sort(key=lambda x:len(x),reverse=True)
+    for view in views:
+        if(view!='发现' and view!='阳光' and view !='标志' and view!='雷凌双擎'):
+            View_First.write(view+'\n')
+
+#根据视角词表是否会被jeiba切开分为NormalViws：会被切开+SpecilaViews:不会被切开
+def SegViews(ViewPathname,NormalName,SpecialName,):
+    normalview=open(NormalName,'w',encoding='utf-8')
+    specialview = open(SpecialName, 'w', encoding='utf-8')
+    label_file = open(ViewPathname, encoding='utf-8')
+    label_data = label_file.readlines()
+    label_file.close()
+    for line in label_data:
+        l1 = line.strip()
+        if((l1.__contains__('-') or l1=='ｍｋ１'  or l1.__contains__('\xa0') or l1.__contains__(' '))):
+            normalview.write(l1 + '\n')
+            continue
+        #是否有中文
+        zhPattern1 = re.compile(u'[\u4e00-\u9fa5]+')
+        match1 = zhPattern1.search(l1)
+        if match1:
+            normalview.write(l1 + '\n')
+        else:
+            specialview.write(l1 + '\n')
+    normalview.close()
+    specialview.close()
+
+def getDict(trainname,labelname,stopWords,pospath,negpath):
+    word_neg_count = {}  # 每个词在负面中出现的次数
+    word_pos_count = {}  # 每个词在正面中出现的次数
+    word_neu_count = {}  # 每个词在中性中出现的次数
+    word_occur = {}  # 每个词在多少个文本中出现，用于计算idf
+    doc_pos_count, doc_neg_count, doc_neu_count = 0, 0, 0  # 文本的情感频数
+    label_ref = {}  # 字典保存label
+    views = {}  # 记录出现过的视角词
+    # 读取训练集输出
+    label = open(labelname, encoding='utf-8')
+    label_data = label.readlines()
+    label.close()
+    for line in label_data:
+        line = line.strip()
+        content = line.split(',')
+        if not len(content) == 3:
+            continue
+        label_id = content[0]
+        label_view = content[1]
+        label_emotion = content[2]
+        views[label_view] = 1
+        if label_ref.get(label_id) is None:
+            label_ref[label_id] = [[label_view, label_emotion]]
+        else:
+            label_ref[label_id].append(
+                [label_view, label_emotion])  # label_ref[id][视角][0]是视角名称，label_ref[id][视角][1]是判断结果
+
+    # 读取训练集输入
+    train = open(trainname, encoding='utf-8')
+    train_data = train.readlines()
+    train.close()
+    for line in train_data:
+        line = line.strip()
+        content = line.split('\t')
+        if not len(content) == 2:
+            continue
+        comment_id = content[0]
+        comment = content[1]
+        # +++++++++++++++++++++++++++++++统计部分++++++++++++++++++++++++++++++++
+        if label_ref.get(comment_id) is None:
+            continue
+        else:
+            view_list = label_ref[comment_id]
+        view_count = len(view_list)  # 读取视角数
+        lines = re.split('。|,|，|:|：|；|\n', comment)
+        dict = {}
+        for view, motion in view_list:
+            dict[view] = ''
+            strour = ''
+            temp = 0
+            for j in range(0, len(lines)):
+                temp_len = 0
+                if (lines[j].__contains__(view)):
+                    strour += lines[j]
+                    dict[view] = strour
+                    temp = j
+                elif (dict[view] != ''):
+                    for s in range(0, view_count):
+                        if ((lines[j].__contains__(view_list[s][0])) == False):
+                            temp_len += 1
+                    if (temp_len == view_count and j > temp):
+                        strour += lines[j]
+                        dict[view] = strour
+                        temp = j
+            dict[view] = dict[view].replace(view, '')
+            words_list = word_filter(dict[view],stopWords)
+            if motion == 'pos':
+                for word in words_list:
+                    word_pos_count[word] = word_pos_count.get(word, 0) + 1
+                    word_occur[word] = word_occur.get(word, 0) + 1
+                doc_pos_count += 1
+            if motion == 'neg':
+                for word in words_list:
+                    word_neg_count[word] = word_neg_count.get(word, 0) + 1
+                    word_occur[word] = word_occur.get(word, 0) + 1
+                doc_neg_count += 1
+            if motion == 'neu':
+                for word in words_list:
+                    word_neu_count[word] = word_neu_count.get(word, 0) + 1
+                    word_occur[word] = word_occur.get(word, 0) + 1
+                doc_neu_count += 1
+    # +++++++++++++++++++++++++++++计算bdc++++++++++++++++++++++++++++++++++
+    # 二分类还是三分类？
+    bdc = {}
+    for word in word_occur:
+        result = 0
+        p_pos = word_pos_count.get(word, 0) / doc_pos_count
+        p_neg = word_neg_count.get(word, 0) / doc_neg_count
+        p_neu = word_neu_count.get(word, 0) / doc_neu_count
+        s = p_neg + p_pos + p_neu
+        if not p_pos == 0:
+            result += (p_pos / s) * math.log2((p_pos / s))
+        if not p_neg == 0:
+            result += (p_neg / s) * math.log2((p_neg / s))
+        if not p_neu == 0:
+            result += (p_neu / s) * math.log2((p_neu / s))
+        result /= math.log2(3)
+        result += 1
+        bdc[word] = result
+
+    # ++++++++++++++++++++++++++++计算tf+++++++++++++++++++++++
+    pos_tf = {}
+    total_pos_words = 0
+    for word in word_pos_count:
+        total_pos_words += word_pos_count.get(word)
+    for word in word_occur:
+        pos_tf[word] = pos_tf.get(word, 0)
+        pos_tf[word] = word_pos_count.get(word, 0) / total_pos_words
+
+    neg_tf = {}
+    total_neg_words = 0
+    for word in word_neg_count:
+        total_neg_words += word_neg_count.get(word)
+    for word in word_occur:
+        neg_tf[word] = neg_tf.get(word, 0)
+        neg_tf[word] = word_neg_count.get(word, 0) / total_neg_words
+
+    # +++++++++++++++++++++++++++++++++++++计算idf+++++++++++++++++++++++++++
+    '''
+    idf = {}
+    for word in word_occur:
+        idf[word] = math.log10((doc_neg_count + doc_neu_count + doc_pos_count) / word_occur.get(word))
+    '''
+    # ++++++++++++++++++++++++++++计算wc+++++++++++++++++++++++
+    word_pos_w = {}
+    word_neg_w = {}
+    word_neu_w = {}
+    for word in word_occur:
+        word_pos_count[word] = word_pos_count.get(word, 0)
+        word_neg_count[word] = word_neg_count.get(word, 0)
+        word_neu_count[word] = word_neu_count.get(word, 0)
+        word_count = word_pos_count.get(word, 0) + word_neg_count.get(word, 0) + word_neu_count.get(word, 0)
+        word_pos_w[word] = word_pos_count.get(word, 0) / word_count
+        word_neg_w[word] = word_neg_count.get(word, 0) / word_count
+        word_neu_w[word] = word_neu_count.get(word, 0) / word_count
+
+    # 直接输出wc按数值排序不为0的
+    pos_wc = []
+    neg_wc = []
+    pos = open(pospath, 'w', encoding='utf-8')
+    neg = open(negpath, 'w', encoding='utf-8')
+    for word in word_occur:
+        # 去除视角词,去除停用词表,然后只要有中文的
+        if (views.get(word) is not None or re.match('^[\u4e00-\u9fa5]+$', word) == None):
+            continue
+        if (word_pos_w[word] > 0):
+            pos_wc.append((word, word_pos_w[word]))
+        if (word_neg_w[word] > 0):
+            neg_wc.append((word, word_neg_w[word]))
+    pos_wc.sort(key=lambda x: x[1], reverse=True)
+    neg_wc.sort(key=lambda x: x[1], reverse=True)
+    for word, weighting in pos_wc:
+        pos.write('%s\t%.2f\n' % (word, weighting))
+    for word, weighting in neg_wc:
+        neg.write('%s\t%.2f\n' % (word, weighting))
+
+#得到停用词表
+def getStopWords(filename,StopName,NormaleViews,SpecialViews):
+    train = open(filename, encoding='utf-8')
+    train_data = train.readlines()
+    stopWords={}
+    pathName = open(StopName, 'w', encoding='utf-8')
+    ImportangWords = {}
+    important_words = ['ssss', 'fashion', 'dreamcar']
+    for word in important_words:
+        ImportangWords[word]=1
+    for line in train_data:
+        line = line.strip()
+        line = line.split('\t')
+        if not len(line) == 2:
+            continue
+        comment = line[1]
+        views = getViews(comment, NormaleViews, SpecialViews)
+        for view in views:
+            comment=comment.replace(view,'')
+        words = jieba.posseg.cut(comment)
+        for word,flag in words:
+            #不要的;x u p q m p
+            if(stopWords.get(word) is None and (flag=='m' or flag=='x' or flag=='p' or flag=='q' or flag=='u' or re.match('^[0-9a-zA-Z]+$',word))):
+                stopWords[word]=1
+                if (ImportangWords.get(word) is None):
+                    pathName.write(word + '\n')
+
+#利用snownlp,得到数据集中每个句子的情感为积极的概率，输出文件格式为：句子ID+句子情感为积极的概率
+def get_SentenceSentiment(filename,pathname):
+    label_file = open(filename, encoding='utf-8')
+    label_data = label_file.readlines()
+    label_file.close()
+    result = open(pathname, 'w', encoding='utf-8')
+    for line in label_data:
+        line = line.strip()
+        line = line.split('\t')
+        s = SnowNLP(line[1])
+        result.write('%s\t%.8f\n' % (line[0], s.sentiments))
+
+#得到情感词典
+NormaleViews=load_table('data/NormalViews.csv',1)
+SpecialViews = load_dict('data/SpecialViews.csv')
+#输入的文件名称
+trainname='data/Train.csv'
+labelname='data/Label.csv'
+testname='data/Test.csv'
+#输出的停用词表的文件名称
+StopName='data/Test_Stopwords.csv'
+#输出的情感词典的文件名称
+pospath='data/pos_dict.csv'
+negpath='data/neg_dict.csv'
+#各种视角词文件名称
+Allviews='data/AllViews.csv'
+ViewPathname='data/Views.csv'
+NormalName='data/NormalViews.csv'
+SpecialName='data/SpecialViews.csv'
+#句子的情感概率值文件名称
+SentenceName='data/TrainSentiment.csv'
+#主要是得到视角词，输入是AllViews,输出是Views+SpecialViews+NormalViews
+iTime=time.time()
+#views=getFirstView(Allviews)
+eTime=time.time()
+print("得到基础视角表总共用时：" + str(eTime - iTime))
+iTime=time.time()
+#getSecondView(views,testname,ViewPathname)
+eTime=time.time()
+print("得到总视角表总共用时：" + str(eTime - iTime))
+iTime=time.time()
+#SegViews(ViewPathname,NormalName,SpecialName,)
+eTime=time.time()
+print("拆分总视角表总共用时：" + str(eTime - iTime))
+#得到停用词表，输入是数据集+SpecialViews+NormalViews，输入是StopWords.csv
+itime=time.time()
+getStopWords(trainname,StopName,NormaleViews,SpecialViews)
+etime=time.time()
+print('得到停用词表总共用时:'+str(etime-itime))
+#得到情感词典，输入是文件名称+停用词表，输出是积极情感词典+消极情感词典
+stopWords = load_dict(StopName)
+itime=time.time()
+#getDict(filename,labelname,stopWords,pospath,negpath)
+etime=time.time()
+print('得到情感词典总共用时:'+str(etime-itime))
+iTime=time.time()
+#get_SentenceSentiment(trainname,SentenceName)
+eTime=time.time()
+print("得到句子的情感概率值总共用时：" + str(eTime - iTime))
---
+++ classifier.py
@@ -0,0 +1,326 @@
+from sklearn.ensemble import GradientBoostingClassifier
+import time
+from methods import *
+from word2vec import *
+import jieba
+
+# 读取snownlp的情感分析结果
+senti_dict = {}
+train_sentiment = open('data/TrainSentiment.csv', encoding='utf-8')
+sentiment_data = train_sentiment.readlines()
+train_sentiment.close()
+for ttt in sentiment_data:
+    l1 = ttt.strip()
+    l2 = l1.split('\t')
+    if not len(l2) == 2:
+        continue
+    senti_dict[l2[0]] = float(l2[1])
+
+# 读取二元组权重
+bigram_pos_dict = {}
+bigram_neg_dict = {}
+bi_sentiment = open('data/bigram_sentiment.csv', encoding='utf-8')
+bi_data = bi_sentiment.readlines()
+bi_sentiment.close()
+for ttt in bi_data:
+    l1 = ttt.strip()
+    l2 = l1.split(',')
+    if not len(l2) == 5:
+        continue
+    bigram_pos_dict[l2[0]] = float(l2[3])
+    bigram_neg_dict[l2[0]] = float(l2[4])
+
+# 读取三元组权重
+thigram_pos_dict = {}
+thigram_neg_dict = {}
+thi_sentiment = open('data/thigram_sentiment.csv', encoding='utf-8')
+thi_data = thi_sentiment.readlines()
+thi_sentiment.close()
+for ttt in thi_data:
+    l1 = ttt.strip()
+    l2 = l1.split(',')
+    if not len(l2) == 5:
+        continue
+    thigram_pos_dict[l2[0]] = float(l2[3])
+    thigram_neg_dict[l2[0]] = float(l2[4])
+
+# 读取单词权重
+unigram_pos_dict = {}
+unigram_neg_dict = {}
+uni_sentiment = open('data/unigram_weight.csv', encoding='utf-8')
+uni_data = uni_sentiment.readlines()
+uni_sentiment.close()
+for ttt in uni_data:
+    l1 = ttt.strip()
+    l2 = l1.split('\t')
+    if not len(l2) == 3:
+        continue
+    unigram_pos_dict[l2[0]] = float(l2[1])
+    unigram_neg_dict[l2[0]] = float(l2[2]) * (193.33 / 266.31)
+
+
+def create_training_set(num_of_train):
+    result_X = []         # 结果
+    result_Y = []         # 分类结果
+
+    # 加载停用词表
+    StopWords = load_dict('data/StopWords.txt')
+
+    # 加载label
+    label_ref = {}
+    label = open('data/Label.csv', encoding='utf-8')
+    label_data = label.readlines()
+    label.close()
+    for ooo in label_data:
+        ooo = ooo.strip()
+        content = ooo.split('\t')
+        if not len(content) == 3:
+            continue
+        label_id = content[0]
+        label_view = content[1]
+        label_emotion = content[2]
+        if label_ref.get(label_id) is None:
+            label_ref[label_id] = [[label_view, label_emotion]]
+        else:
+            label_ref[label_id].append([label_view, label_emotion])
+
+    # 加载评论
+    train = open('data/Train.csv', encoding='utf-8')
+    train_data = train.readlines()
+    train.close()
+    this_line_count = 1
+    for ooo in train_data:
+        if this_line_count > num_of_train:
+            break
+        ooo = ooo.strip()
+        content = ooo.split('\t')
+        if not len(content) == 2:
+            continue
+        this_comment_id = content[0]
+        this_comment = content[1]
+        if label_ref.get(this_comment_id) is None:
+            continue
+        else:
+            view_list = label_ref[this_comment_id]
+        view_count = len(view_list)  # 读取视角数
+
+        this_lines = re.split('。|,|，|:|：|；|\n', this_comment)
+        for this_view, motion in view_list:
+            strour = ''
+            temp = 0
+            for j in range(len(this_lines)):
+                temp_len = 0
+                if this_view in this_lines[j]:
+                    strour += this_lines[j]
+                    temp = j
+                elif strour != '':
+                    for s in range(view_count):
+                        if not view_list[s][0] in this_lines[j]:
+                            temp_len += 1
+                    if temp_len == view_count and j > temp:
+                        strour += this_lines[j]
+                        temp = j
+
+            # 去停前识别二元组
+            this_unfiltered = jieba.cut(strour)
+            this_unfiltered_list = []
+            for this_foo in this_unfiltered:
+                this_unfiltered_list.append(this_foo)
+            this_pos_bi_feature = 0.
+            this_neg_bi_feature = 0.
+            this_count = 0
+            for this_pointer in range(len(this_unfiltered_list) - 1):
+                this_bigram = this_unfiltered_list[this_pointer] + '^' + this_unfiltered_list[this_pointer + 1]
+                if not bigram_pos_dict.get(this_bigram) is None:
+                    this_pos_bi_feature += bigram_pos_dict[this_bigram]
+                    this_neg_bi_feature += bigram_neg_dict[this_bigram]
+                    this_count += 1
+            if not this_count == 0:
+                this_pos_bi_feature = this_pos_bi_feature / this_count
+                this_neg_bi_feature = this_neg_bi_feature / this_count
+
+            # 识别三元组
+            this_pos_thi_feature = 0.
+            this_neg_thi_feature = 0.
+            this_count = 0
+            for this_pointer in range(len(this_unfiltered_list) - 2):
+                this_thigram = this_unfiltered_list[this_pointer] + '^' + this_unfiltered_list[this_pointer + 1] + \
+                               '^' + this_unfiltered_list[this_pointer+2]
+                if not thigram_pos_dict.get(this_thigram) is None:
+                    this_pos_thi_feature += thigram_pos_dict[this_thigram]
+                    this_neg_thi_feature += thigram_neg_dict[this_thigram]
+                    this_count += 1
+            if not this_count == 0:
+                this_pos_thi_feature = this_pos_thi_feature / this_count
+                this_neg_thi_feature = this_neg_thi_feature / this_count
+
+            this_words_list = word_filter(strour, StopWords)
+
+            # 单词在去停后识别
+            this_pos_uni_feature = 0.
+            this_neg_uni_feature = 0.
+            this_count = 0
+            for this_word in this_words_list:
+                if not unigram_pos_dict.get(this_word) is None:
+                    this_pos_uni_feature += unigram_pos_dict[this_word]
+                    this_neg_uni_feature += unigram_neg_dict[this_word]
+                    this_count += 1
+            if not this_count == 0:
+                this_pos_uni_feature = this_pos_uni_feature / this_count
+                this_neg_uni_feature = this_neg_uni_feature / this_count
+
+            this_word_vec = get_vec(this_words_list)                          # 词向量200维
+            this_snownlp_result = senti_dict[this_comment_id]
+            this_word_vec.append(this_snownlp_result)                        # 1维snownlp情感分析整个句子结果
+            this_word_vec.append(this_pos_bi_feature - this_neg_bi_feature)                        # 1维2元组正面概率
+            this_word_vec.append(this_pos_thi_feature - this_neg_thi_feature)                      # 1维3元组正面概率
+            this_word_vec.append(this_pos_uni_feature - this_neg_uni_feature)                      # 1维单词正面概率
+            result_X.append(this_word_vec)        # 特征为204维
+            if motion == 'pos':
+                result_Y.append(1)
+            elif motion == 'neg':
+                result_Y.append(-1)
+            else:
+                result_Y.append(0)
+        this_line_count += 1
+    return result_X, result_Y
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+TOTAL_NUMBER_OF_TRAIN = 14533
+NUM_OF_TRAIN = 10000
+TEST_FILE = 'Train.csv'
+OUTPUT_FILE = 'result_3.csv'
+itime = time.time()
+
+# 加载词典
+NormaleViews = load_table('data/NormalViews.csv', 1)
+SpecialViews = load_dict('data/SpecialViews.csv')
+StopWords = load_dict('data/StopWords.txt')
+
+# 初始化分类器
+clf = GradientBoostingClassifier(n_estimators=400)
+# 将测试对象转化为向量
+
+train_x, train_y = create_training_set(NUM_OF_TRAIN)
+
+clf.fit(train_x, train_y)
+t = time.time()
+print('训练完成共用时'+str(t-itime))
+test_file = open(('data/'+TEST_FILE), encoding='utf-8')
+test_data = test_file.readlines()
+test_file.close()
+result_file = open(('result/'+OUTPUT_FILE), 'w', encoding='utf-8')
+line_count = 0
+
+# 读取snownlp的结果
+sentiment_dict = {}
+test_sentiment = open('data/TrainSentiment.csv', encoding='utf-8')
+sentiment_data = test_sentiment.readlines()
+test_sentiment.close()
+for jjj in sentiment_data:
+    l1 = jjj.strip()
+    l2 = l1.split('\t')
+    if not len(l2) == 2:
+        continue
+    sentiment_dict[l2[0]] = float(l2[1])
+
+for line in test_data:
+    line_count += 1
+    if not line_count > NUM_OF_TRAIN:
+        continue
+    l1 = line.strip()
+    l2 = l1.split('\t')
+    if not len(l2) == 2:
+        continue
+    comment_id = l2[0]
+    comment = l2[1]
+    # 识别视角
+    views = getViews(comment, NormaleViews, SpecialViews)
+
+    # 规则？
+    # 用于存放经过规则处理之后应删除的视角
+    remove_views = []
+    # 处理比较句
+    compViews = getCompSentence(views, l2[1])
+    for comview in compViews:
+        result_file.write('%s\t%s\t%d\n' % (comment_id, comview, 0))
+        remove_views.append(comview)
+    # 把比较句中的视角词删除了，避免有些视角词同时出现在特殊句式中，这里还有个优先级待做
+    for view in remove_views:
+        views.remove(view)
+    remove_views = []
+    # 处理反比句
+    comviews = getInverseSen(views, l2[1])
+    for comview in comviews:
+        result_file.write('%s\t%s\t%d\n' % (comment_id, comview, 0))
+        remove_views.append(comview)
+    for view in remove_views:
+        views.remove(view)
+    if views == []:
+        continue
+
+    # 分类器判断
+    lines = re.split('。|,|，|:|：|；|\n', l2[1])
+    for i in range(len(views)):
+        sentence = getSentence(views[i], views, lines)
+
+        # 去停前识别bigram
+        unfiltered = jieba.cut(sentence)
+        unfiltered_list = []
+        for foo in unfiltered:
+            unfiltered_list.append(foo)
+        pos_bi_feature = 0.
+        neg_bi_feature = 0.
+        count = 0
+        for pointer in range(len(unfiltered_list) - 1):
+            bigram = unfiltered_list[pointer] + '^' + unfiltered_list[pointer + 1]
+            if not bigram_pos_dict.get(bigram) is None:
+                pos_bi_feature += bigram_pos_dict[bigram]
+                neg_bi_feature += bigram_neg_dict[bigram]
+                count += 1
+        if not count == 0:
+            pos_bi_feature = pos_bi_feature / count
+            neg_bi_feature = neg_bi_feature / count
+
+        # 识别三元组
+        pos_thi_feature = 0.
+        neg_thi_feature = 0.
+        count = 0
+        for pointer in range(len(unfiltered_list) - 2):
+            thigram = unfiltered_list[pointer] + '^' + unfiltered_list[pointer + 1] + '^' + \
+                      unfiltered_list[pointer + 2]
+            if not thigram_pos_dict.get(thigram) is None:
+                pos_thi_feature += thigram_pos_dict[thigram]
+                neg_thi_feature += thigram_neg_dict[thigram]
+                count += 1
+        if not count == 0:
+            pos_thi_feature = pos_thi_feature / count
+            neg_thi_feature = neg_thi_feature / count
+
+        words_list = word_filter(sentence, StopWords)
+
+        # 单词在去停后识别
+        pos_uni_feature = 0.
+        neg_uni_feature = 0.
+        count = 0
+        for word in words_list:
+            if not unigram_pos_dict.get(word) is None:
+                pos_uni_feature += unigram_pos_dict[word]
+                neg_uni_feature += unigram_neg_dict[word]
+                count += 1
+        if not count == 0:
+            pos_uni_feature = pos_uni_feature / count
+            neg_uni_feature = neg_uni_feature / count
+
+        word_vec = get_vec(words_list)                              # 200维词向量
+        snownlp_result = sentiment_dict[comment_id]                 # snownlp情感分析结果作为1个维度
+        word_vec.append(snownlp_result)
+        word_vec.append(pos_bi_feature - neg_bi_feature)
+        word_vec.append(pos_thi_feature - neg_thi_feature)
+        word_vec.append(pos_uni_feature - neg_uni_feature)
+        test_x = [word_vec]
+        test_y = clf.predict(test_x)
+        result_file.write('%s\t%s\t%d\n' % (comment_id, views[i], test_y[0]))
+result_file.close()
+t = time.time()
+print('共用时'+str(t-itime))
