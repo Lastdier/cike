a3940af2612deafb77a081c877c9eaa0b35f20b1
Lastdier <a499340016@gmail.com>
2016-12-05 23:16:50 +0800

    word2vec作200维度词向量，snownlp情感分析提供1个特征维度，bigram、thigram、unigram各提供1个特征维度，用GBDT进行三分类 线下f1: 76.9

--- README.md
+++ README.md
@@ -1,26 +1,10 @@
 # cike_guoshuang
 
-wighting 生成权重////////
-filter 过滤掉权重词典中的视角词///////
-convert_dict 将Excel输出的csv转换成utf-8///////
+methods 存放所有方法////////
+PreProcess 这个主要是做预处理，得到停用词表+情感词典+视角词表///////
 create_training_set 根据词典将训练集构造成向量/////////
 classificator 训练SVM并预测测试数据/////////
 merge 将挑选正面的结果和挑选负面的结果组合/////////
+filter_result 这个纯粹是为了提高最后正确率，将Train和Test中相同的找出来，修正Label，
+个人有疑问就是机器学习应该两个相同的句子特征向量是一样的，为何还会有不一样的结果？/////////
 check_file 检验结果的F1值
-
-修改:
-1.methods.py,更换为存储其他.py用到的方法
-2.creat_trainning_set.py,更换为按照,.;?等切分句子,视角词所在的分句的情感词+无视角的句子所在的情感词作为视角词的情感词
-3.classificator.py修改为直接判断句子中所有的视角词,然后判断每个视角词的情感,先分为对比句,反比句,疑问句
-4.删除Label_Test.csv,直接在check_file.py那里设置测试样本有多少行
-新增:
-1.getStopWords.py,按照词性得到p,q,m,u,作为停用词表(感觉这个可能不需要了)
-2.getView.py,之前的所有的视角词作为基础视角词表,按照后缀是英文/数字等拓展
-待做:
-1.合并wighting.py+filter.py+convert_dict.py直接从weighting从抽取出特征,得到情感词典
-2.将规则和机器学习结合在一起,问题是样本要怎么分?
-3.测试了不同的视角词,基于规则的方法,结果从0.45-0.54不等,接下来寻找拓展视角词的办法,讲find_more_view.py完善
-
-
-
-
--- methods.py
+++ methods.py
@@ -1,138 +1,215 @@
-import jieba.posseg
+import jieba
 import re
-jieba.load_userdict('data/NormalViewdict.csv')
-words_that_should_not_be_divided = {'召回','自燃','短轴','看上去','EA1111.4T','大帕萨特','B7.5','高尔夫大使','女子高尔夫',
-                                    '5000','AS5174','PM2.5',
-        '奥迪量产','DL382','中华区','PLA3.0','申联阳光','双龙大道','双龙山森林公园','双龙寺','大众汽车集团',
-        '捷豹路虎汽车','EA211','大本田','ESP','特斯拉杀手','1.4TFSI','大众style','小辉腾','EVO150.4','V6发动机',
-        'v6发动机','NEX12.2','MC261','200T','变形金刚','小康生活','东风标志','PassatA','媒体大众','350牛米','现代感',
-        '标志性','马丁脸','前麦弗逊','现代化','PQ34','EA88','PQ35','荣威新能源','马自达工厂','上海通用工厂',
-        '捷克姆拉达','标致大道','大众一家','上海大众汽车','大众赛车','C级车',
-        'C级豪华','C级轿跑','C级别','高尔夫运动','别克4S店', '长安', '小夏', '长城', '大切', '凯泽西', '发现四', '大宝',
-        '小理', '小四', '小海', '大发', '大奔', '大众', '小八', '拉达', '小七', '小科', '老马', '长马', '汉能', '迷你',
-        '起亚'}
-dict_file = open('data/NormalViewdict.csv', encoding='utf-8')
-dict_data = dict_file.readlines()
-dict_file.close()
-for i in dict_data:
-    i1 = i.strip()
-    i2 = i1.split(' ')
-    words_that_should_not_be_divided.add(i2[0])
 
-addition_file_1 = open('data/view_1107.csv', encoding='utf-8')
-addition_data_1 = addition_file_1.readlines()
-addition_file_1.close()
-for i in addition_data_1:
-    i1 = i.strip()
-    words_that_should_not_be_divided.add(i1)
+# 加载特殊视角词表
+def load_table(source,type):
+    path=open(source, encoding='utf-8')
+    file=path.readlines()
+    path.close()
+    view=[]
+    if(type==1):
+        for line in file:
+            line = line.strip()
+            view.append(line)
+    if(type==0):
+        for line in file:
+            line = line.strip()
+            line=line.split('\t')
+            view.append(line[0])
+    return view
 
-addition_file_2 = open('data/new_view.csv', encoding='utf-8')
-addition_data_2 = addition_file_2.readlines()
-addition_file_2.close()
-for i in addition_data_2:
-    i1 = i.strip()
-    words_that_should_not_be_divided.add(i1)
+# 反比句
+def getInverseSen(views, line):
+    comviews = []
+    for view in views:
+        s1 = '不如' + view
+        if (line.__contains__(s1)):
+            comviews.append(view)
+    return comviews
 
-for l in words_that_should_not_be_divided:
-    jieba.suggest_freq(l, True)
+# 比较句
+def getCompSentence(views, line):
+    comviews = []
+    for view in views:
+        s1 = '与' + view + '相比'
+        s2 = '比' + view
+        s3 = '超越' + view
+        if (line.__contains__(s1)):
+            comviews.append(view)
+        elif (line.__contains__(s2)):
+            comviews.append(view)
+        elif (line.__contains__(s3)):
+            comviews.append(view)
+    return comviews
 
+def listTostring(list):
+    outStr = ' '
+    for word in list:
+        outStr += word
+        outStr += ' '
+    return outStr
 
-# 将句子中有歧义的词，带单位的数字删除
-def preProcess(this_str):
-    # 处理产生歧义的词
+#处理含中文的视角词
+def preNormalViews(str):
+    #处理产生歧义的词
     p = r".*(现代.*传统).*|.*(传统.*现代).*|.*(现代.*时尚).*|.*(时尚.*现代).*|.*(现代.*科技).*|.*(科技.*现代).*|.*(音乐.*现代).*|.*(现代.*音乐).*|.*(现代.*经典).*|.*(经典.*现代).*|.*(现代.*绅士).*|.*(绅士.*现代).*|.*(现代.*文明).*|.*(文明.*现代).*|.*(现代.*服务).*|.*(服务.*现代).*.*(现代.*中国).*|.*(中国.*现代).*|.*(现代.*文明).*|.*(文明.*现代).*|.*(现代.*农业).*|.*(农业.*现代).*|.*(现代.*教育).*|.*(教育.*现代).*|.*(现代.*美).*|.*(美.*现代).*|.*(现代.*人).*|.*(人.*现代).*"
-    if len(re.findall(p, this_str)) > 0:
-        this_str = this_str.replace('现代', '')
+    if len(re.findall(p, str)) > 0:
+        str = str.replace('现代', '')
     lists=['汽车','狗狗Polo','大火中的Polo','1.4TEA211','长安宝宝','迈腾全国猪价资讯','XDS','Cross Lavida','Der Yeti in Berlin','Yak Yeti酒店','别克制','高尔夫度假酒店','','']
     # 现代出现了557次，然后有很多会导致歧义！
     xiandai = ['现代消费者','更现代','期待现代','现代学徒','现代奥运会','现代企业','现代露天剧场','现代风格','现代化','现代人','现代龙泉青','现代生活','既现代','融入现代','组成现代','现代文化','现代文明','现代交通安全','现代五项队','现代职业教育','现代豪华','现代艺术','现代诗歌','近现代','后现代','现代感','现代天地','现代豪华','现代制药','现代农业','现代都市','现代艺术','现代诗歌','近现代','后现代','现代感','现代天地','现代豪华','现代制药','现代农业','现代都市','回现代']
     # 大众出现了300+次，然后有很多会导致歧义！
     dazhong = ['大众旅游','大众资讯','大众喜爱','330御尊','长安福特店','30E','H5FF408','280TSI']
-    this_str = replace(this_str, lists)
-    this_str = replace(this_str, xiandai)
-    this_str = replace(this_str, dazhong)
-    this_str = this_str.replace('全新帕萨特', '帕萨特')
-    this_str = this_str.replace('新帕萨特', '帕萨特')
-    this_str = this_str.replace('小夏利', '夏利')
-    this_str = this_str.replace('一汽吉林4S店', '一汽')
-    this_str = this_str.replace('比速腾', '速腾')
-    this_str = this_str.replace('进口大众尚酷DSG', '大众尚酷DSG')
-    return this_str
+    str = replace(str, lists)
+    str = replace(str, xiandai)
+    str = replace(str, dazhong)
+    str = str.replace('一汽吉林4S店', '一汽')
+    str = str.replace('比速腾', '速腾')
+    lists = ['1.4TEA211', 'Yak Yeti酒店', '别克制', '高尔夫度假酒店', '唐山', '唐僧', '唐太宗', '唐古', '后唐', '唐都', '唐侯', '唐斌',
+             '唐唯实', '展唐科技', '唐装', '唐朝', '盛唐改装', '唐河','suv','科技']
+    str = replace(str, lists)
+    str = str.replace('小夏利', '夏利')
+    str = str.replace('小夏朗', '夏朗')
+    str = str.replace('比速腾', '速腾')
+    str = str.replace('大众泰', '众泰')
+    str = str.replace('上海通用汽车', '上海通用')
+    str = str.replace('进口大众尚酷DSG', '大众尚酷DSG')
+    str = str.replace('[起亚律动]', '')
+    str = str.replace('变形金刚', '')
+    str = str.replace('雷克萨斯rx200t', '雷克萨斯rx')
+    return str
 
+#处理含中文的视角词
+def preSpecialViews(str):
+    # 处理带有单位的数字
+    lists = ['qq群', 'qq号', '@qq']
+    str = replace(str, lists)
+    pattern = r'\d+[元|万|转|幅|马|款|米|年|多|人|台|辆|名|公|月]|\d+rpm|\d+公里|\d+积分|\d+月\d+日|\d+n·m|\d\.\d\t|\d\.\d排量|\d\.\dl|\d+\-\d+'
+    for word in re.findall(pattern, str):
+        str = str.replace(word, '数量')
+    return str
 
-# 删除句子中会产生歧义的词，比如现代化，可能会被是被为视角词;现代
-def replace(this_str, lists):
+def getMotion(result,pos,neg):
+    count=0
+    for word in result:
+        if(word in pos):
+            count+=1
+        if(word in neg):
+            count-=1
+    motionValue='neu'
+    if(count>0):
+        motionValue='pos'
+    elif(count<0):
+        motionValue='neg'
+    return motionValue
+
+#去除不要的词
+def replace(str,lists):
     for word in lists:
-        if word in this_str:
-            this_str = this_str.replace(word, '')
-    return this_str
+        str=str.replace(word,'')
+    return str
 
-
-# j加载特殊视角词表
-def load_table(source):
-    file = open(source, encoding='utf-8').readlines()
-    view = []
+#加载字典
+def load_dict(source):
+    path=open(source, encoding='utf-8')
+    file=path.readlines()
+    path.close()
+    view={}
     for line in file:
         line = line.strip()
-        view.append(line)
+        view[line]=1
     return view
 
 
 # 删除不需要的词，将剩余的词转换为list
-def word_filter(string):
-    cache = {}
-    result = []                             # 以list装载数据
-    string = preProcess(string)
-    path = 'data/SpecialView.csv'           # 这个文件里保存分词前要识别的视角词
-    new_view = load_table(path)
-    marker_set = ['☢', '☣', '☤', '☥', '☦', '☧', '☨', '☩', '☪', '☫', '☬', '☭', '☮', '☯', '☰', '☱', '☲', '☳', '☴', '☵', '☶', '☷', '☸']              # 用来标记视角的占位符
-
-    pointer = 0
-    for view in new_view:
-        if view in string:
-            cache[marker_set[pointer]] = view
-            string = string.replace(view, marker_set[pointer])             # 先识别分词器无法区分的视角，再用特殊标记标记出来
-            pointer += 1
-    words = jieba.posseg.cut(string)
-
-    for word, flag in words:
-        if 'u' in flag:
-            pass
-        elif 'p' == flag:
-            pass
-        elif 'q' == flag:
-            pass
-        elif 'm' == flag:
-            pass
-        elif word in marker_set:
-            result.append(cache[word])
-        else:
+def word_filter(string, StopWords):
+    result = []
+    words = jieba.cut(string)
+    for word in words:
+        if StopWords.get(word) is None:
             result.append(word)
     return result
+
+
+#得到合并的视角词,比如:a4q5D3
+def getSpecilaView(str,SpecialViews):
+    views=[]
+    SpecialView=list(SpecialViews)
+    SpecialView.sort(key=lambda x: len(x), reverse=True)
+    for view in SpecialView:
+        if(str.__contains__(view)):
+            str=str.replace(view,'')
+            views.append(view)
+    if(str=='' or str.isdigit()):
+        pass
+    else:
+        views=[]
+    return views
 
 
 def search_features_and_wight(word_list, features_list):
     num_of_features = len(features_list)
     result = [0.] * num_of_features
     for word in word_list:
-        for fea in features_list:
-            if fea[0] == word:
-                result[features_list.index(fea)] += float(fea[1])
+        if not features_list.get(word) is None:
+            result[features_list[word][0]] += float(features_list[word][1])
     return result
 
 
+# 加载字典
 def get_features(dict_name, k):
-    # 加载字典
     my_dict = open('data/' + dict_name, encoding='utf-8')
     data = my_dict.readlines()
     my_dict.close()
-    features = []
-    count = 1
+    features = {}
+    count = 0
     for line in data:
-        if count > k:
+        if((count+1)>k):
             break
         l1 = line.strip()
         this_line = l1.split('\t')
-        features.append([this_line[0], this_line[1]])
+        features[this_line[0]]= [count,this_line[1]]
         count += 1
     return features
+
+#得到句子中的所有视角词
+def getViews(comment,NormaleViews,SpecialViews):
+    views = []
+    # 处理中文的歧义视角
+    comment = preNormalViews(comment)
+    # 找出普通视角词
+    for view in NormaleViews:
+        if (comment.__contains__(view) == True):
+            views.append(view)
+            comment = comment.replace(view, '')
+    # 处理含数字的歧义视角
+    comment=preSpecialViews(comment)
+    temp_views = jieba.cut(comment)
+    for word in temp_views:
+        if (SpecialViews.get(word) is not None):
+            views.append(word)
+        else:
+            # 处理英文+数字的视角
+            if ((re.match('^[0-9a-zA-Z]+$', word)) and word.isalpha() == False and (word.isdigit() == False)):
+                for view in getSpecilaView(word, SpecialViews):
+                    views.append(view)
+    views = list(set(views))
+    return views
+
+#得到含有视角词的句子以及无视角词的句子
+def getSentence(view,views,lines):
+    strour = ''
+    temp = 0
+    for j in range(0, len(lines)):
+        temp_len = 0
+        if (lines[j].__contains__(view)):
+            strour += lines[j]
+            temp = j
+        elif (strour!= ''):
+            for s in range(0, len(views)):
+                if ((lines[j].__contains__(views[s])) == False):
+                    temp_len += 1
+            if (temp_len == len(views) and j > temp):
+                strour += lines[j]
+                temp = j
+    return strour
